56d18e0b5789ff513fa970ce889e74745394b4ea
maviva97@gmail.com <maviva97@gmail.com>
2023-10-11 12:42:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+Data d=new Data();
+d.setZi(32);
+d.setLuna(13);
+d.setAn(2012);
+        System.out.println(d.getZi()+"/"+d.getLuna()+"/"+d.getAn());
+
+    }
+}
